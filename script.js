b38d6ca806308717bd831c207abcf0d9f48af193
Omar Rosales <40807858+Omar-Rosales@users.noreply.github.com>
2023-10-03 18:12:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Add any JavaScript code for interactivity
+document.addEventListener("DOMContentLoaded", function() {
+  const publicationsSection = document.getElementById("publications");
+  const publicationListItems = publicationsSection.querySelectorAll("li");
+
+  publicationListItems.forEach((li) => {
+    li.innerHTML = li.innerHTML.replace(/Rosales, O./g, '<strong>Rosales, O.</strong>');
+  });
+});
